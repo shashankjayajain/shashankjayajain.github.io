fab9bbdc06c5bc0376055aaee497c931a20c972c
UpfrontSecurity <shashankrehul@gmai.com>
2019-06-10 12:21:10 +0530

    BSN test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -13,3 +13,69 @@ firebase.initializeApp({
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
 const messaging = firebase.messaging();
+
+/*messaging.setBackgroundMessageHandler(function(payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+});*/
+
+
+//-----------------------------------------------------------------------------------------
+
+/*self.addEventListener('notificationclick', function(event) {
+    console.log('On notification click: ', event.notification.data.url);
+    // Android doesn’t close the notification when you click on it
+    // See: http://crbug.com/463146
+    event.notification.close();
+
+    // This looks to see if the current is already open and
+    // focuses if it is
+    console.log('Notification click: data.url ', event.notification.data.url);
+    event.notification.close();
+    var url = /localhost:3999|dev-piquemeup.firebaseapp.com/;
+    var newurl = "/chat";
+    if (event.notification.data.url) {
+        newurl = event.notification.data.url;
+    }
+
+    function endsWith(str, suffix) {
+        return str.indexOf(suffix, str.length - suffix.length) !== -1;
+    }
+
+    event.waitUntil(
+        clients.matchAll({
+            type: 'window'
+        })
+            .then(function(windowClients) {
+                for (var i = 0; i < windowClients.length; i++) {
+                    var client = windowClients[i];
+                    if (url.test(client.url) && 'focus' in client) {
+                        if (endsWith(client.url, "/app.html#" + newurl)) {
+                            console.log("******** Yes it matched *******");
+                            return client.focus();
+                        }
+                        return client.navigate("/app.html#" + newurl)
+                            .then(client => client.focus());
+                    }
+                }
+                if (clients.openWindow) {
+                    return clients.openWindow("/app.html#" + newurl);
+                }
+            })
+    );
+
+});*/
+
+//===========================================================================================
+
+messaging.setBackgroundMessageHandler(function(payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    var notificationTitle = 'Background Message Title';
+    var notificationOptions = {
+        body: 'Background Message body.',
+        icon: '/firebase-logo.png'
+    };
+
+    return self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
