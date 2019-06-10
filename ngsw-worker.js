f84ac23d02601259c841a709e213f1eb6af34073
UpfrontSecurity <shashankrehul@gmai.com>
2019-06-10 12:34:25 +0530

    BSN test

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1883,7 +1883,6 @@ ${msgIdle}`, { headers: this.adapter.newHeaders({ 'Content-Type': 'text/plain' }
             this.scope.addEventListener('message', (event) => this.onMessage(event));
             this.scope.addEventListener('push', (event) => this.onPush(event));
             this.scope.addEventListener('notificationclick', (event) => this.onClick(event));
-
             // The debugger generates debug pages in response to debugging requests.
             this.debugger = new DebugHandler(this, this.adapter);
             // The IdleScheduler will execute idle tasks after a given delay.
